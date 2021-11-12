074dc2f54e5461eb792743f01491a2121511f8bf
Kaneyo <tomduchesne46@gmail.com>
2021-11-12 11:56:01 +0100

    Fin TP GildedRose

--- gildedrose/src/main/java/fr/unilim/iut/gildedrose/LegacyInn.java
+++ gildedrose/src/main/java/fr/unilim/iut/gildedrose/LegacyInn.java
@@ -3,10 +3,10 @@ package fr.unilim.iut.gildedrose;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inn {
+public class LegacyInn {
   private List<Item> items;
 
-  public Inn() {
+  public LegacyInn() {
     items = new ArrayList<Item>();
     items.add(new Item("+5 Dexterity Vest", 10, 20));
     items.add(new Item("Aged Brie", 2, 0));
@@ -23,25 +23,25 @@ public class Inn {
   public void updateQuality() {
     for (int i = 0; i < items.size(); i++) {
       if (!items.get(i).getName().equals("Aged Brie") && !items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (qualitéItem(i) > 0) {
+        if (items.get(i).getQuality() > 0) {
           if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-            items.get(i).setQuality(qualitéItem(i) - 1);
+            items.get(i).setQuality(items.get(i).getQuality() - 1);
           }
         }
       } else {
-        if (qualitéItem(i) < 50) {
-          items.get(i).setQuality(qualitéItem(i) + 1);
+        if (items.get(i).getQuality() < 50) {
+          items.get(i).setQuality(items.get(i).getQuality() + 1);
 
           if (items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (items.get(i).getSellIn() < 11) {
-              if (qualitéItem(i) < 50) {
-                items.get(i).setQuality(qualitéItem(i) + 1);
+              if (items.get(i).getQuality() < 50) {
+                items.get(i).setQuality(items.get(i).getQuality() + 1);
               }
             }
 
             if (items.get(i).getSellIn() < 6) {
-              if (qualitéItem(i) < 50) {
-                items.get(i).setQuality(qualitéItem(i) + 1);
+              if (items.get(i).getQuality() < 50) {
+                items.get(i).setQuality(items.get(i).getQuality() + 1);
               }
             }
           }
@@ -55,25 +55,21 @@ public class Inn {
       if (items.get(i).getSellIn() < 0) {
         if (!items.get(i).getName().equals("Aged Brie")) {
           if (!items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (qualitéItem(i) > 0) {
+            if (items.get(i).getQuality() > 0) {
               if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-                items.get(i).setQuality(qualitéItem(i) - 1);
+                items.get(i).setQuality(items.get(i).getQuality() - 1);
               }
             }
           } else {
-            items.get(i).setQuality(qualitéItem(i) - qualitéItem(i));
+            items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
           }
         } else {
-          if (qualitéItem(i) < 50) {
-            items.get(i).setQuality(qualitéItem(i) + 1);
+          if (items.get(i).getQuality() < 50) {
+            items.get(i).setQuality(items.get(i).getQuality() + 1);
           }
         }
       }
     }
 
   }
-
-private int qualitéItem(int i) {
-	return items.get(i).getQuality();
-}
 }
--- gildedrose/src/test/java/fr/unilim/iut/gildedrose/InnTest.java
+++ gildedrose/src/test/java/fr/unilim/iut/gildedrose/InnTest.java
@@ -3,8 +3,132 @@ package fr.unilim.iut.gildedrose;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 public class InnTest {
   @Test
-  public void should_write_tests() {
+  public void should_list_items() {
+	  assertThat(new Inn().getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(new Inn().getItems()).extracting("sellIn").containsExactly(10, 2, 5, 0, 15, 3);
+	  assertThat(new Inn().getItems()).extracting("quality").containsExactly(20, 0, 7, 80, 20, 6);
+  } 
+  
+  @Test
+  public void should_update_quality() {
+	 Inn inn = new Inn();
+	 inn.updateQuality();
+	 assertThat(inn.getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(19, 1, 6, 80, 21, 5);
+  } 
+  
+  @Test
+  public void should_update_quality_twice() {
+	  Inn inn = new Inn();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(8, 0, 3, 0, 13, 1);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(18, 2, 5, 80, 22, 4);
+  }
+  
+  @Test
+  public void should_update_quality_thrice() {
+	  Inn inn = new Inn();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(7, -1, 2, 0, 12, 0);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(17, 4, 4, 80, 23, 3);
+  }
+  
+  @Test
+  public void should_update_quality_four_times() {
+	  Inn inn = new Inn();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(6, -2, 1, 0, 11, -1);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(16, 6, 3, 80, 24, 1);
+  }
+  
+  @Test
+  public void should_update_quality_a_lot() {
+	  Inn inn = new Inn();
+	  for (int day = 0; day<100; day++) {
+		  inn.updateQuality();
+	  }
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  	"+5 Dexterity Vest",
+			    "Aged Brie",
+			    "Elixir of the Mongoose",
+			    "Sulfuras, Hand of Ragnaros",
+			    "Backstage passes to a TAFKAL80ETC concert",
+			    "Conjured Mana Cake"
+	  );
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(-90, -98, -95, 0, -85, -97);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(0, 50, 0, 80, 0, 0);
+  }
+  
+  @Test
+  public void should_test_against_legacy_code() {
+	  LegacyInn legacyInn = new LegacyInn();
+	  Inn inn = new Inn();
+	  
+	  for (int day = 0; day < 1000; day++) {
+		  List<Item> items = inn.getItems();
+		  List<Item> legacyItems = legacyInn.getItems();
+
+		  assertThat(items).hasSize(legacyItems.size());
+		  for (int i = 0; i < legacyItems.size(); i++) {
+			  Item item = items.get(i);
+			  Item legacyItem = legacyItems.get(i);
+			  
+			  assertThat(item.getName()).isEqualTo(legacyItem.getName());
+			  assertThat(item.getQuality()).isEqualTo(legacyItem.getQuality());
+			  assertThat(item.getSellIn()).isEqualTo(legacyItem.getSellIn());
+		  }
+		  
+		  inn.updateQuality();
+		  legacyInn.updateQuality();
+	  }
   }
 }
